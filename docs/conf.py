329078654022b87cb8a4ec37fb6c8df566d38123
Duda Nogueira <dudanogueira@gmail.com>
2021-04-14 17:58:14 -0300

    Fix Docs 3

--- docs/conf.py
+++ docs/conf.py
@@ -15,14 +15,14 @@ import sys
 import django
 
 if os.getenv("READTHEDOCS", default=False) == "True":
-    sys.path.insert(0, os.path.abspath(".."))
+    sys.path.append(os.path.abspath(".."))
+    sys.path.append(os.path.abspath("../../"))
     os.environ["DJANGO_READ_DOT_ENV_FILE"] = "True"
     os.environ["USE_DOCKER"] = "no"
 else:
     sys.path.insert(0, os.path.abspath("/app"))
 
-sys.path.insert(os.path.abspath(".."))
-sys.path.append(os.path.abspath("../../"))
+
 
 os.environ["DATABASE_URL"] = "sqlite:///readthedocs.db"
 os.environ["CELERY_BROKER_URL"] = os.getenv("REDIS_URL", "redis://redis:6379")
