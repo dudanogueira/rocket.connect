6f366aa3580e3fc4d40dd859ac4bf26e959c0a4a
Duda Nogueira <dudanogueira@gmail.com>
2021-07-28 15:09:08 -0300

    Add DJANGO_SESSION_COOKIE_SECURE and DJANGO_CSRF_COOKIE_SECURE env variable in order to allow local usage without SSL

--- config/settings/production.py
+++ config/settings/production.py
@@ -36,9 +36,9 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-ssl-redirect
 SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-secure
-SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = env.bool("DJANGO_SESSION_COOKIE_SECURE", default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-secure
-CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
 # https://docs.djangoproject.com/en/dev/topics/security/#ssl-https
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-hsts-seconds
 # set this to 60 seconds first and then to 518400 once you prove the former works
