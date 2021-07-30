b765cea0a803db8c3ae8895693b66c86d4cc6ab9
Duda Nogueira <dudanogueira@gmail.com>
2021-07-30 09:53:38 -0300

    Add DJANGO_CSRF_COOKIE_HTTPONLY environment

--- config/settings/base.py
+++ config/settings/base.py
@@ -209,7 +209,7 @@ FIXTURE_DIRS = (str(APPS_DIR / "fixtures"),)
 # https://docs.djangoproject.com/en/dev/ref/settings/#session-cookie-httponly
 SESSION_COOKIE_HTTPONLY = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#csrf-cookie-httponly
-CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_HTTPONLY = env.bool("DJANGO_CSRF_COOKIE_HTTPONLY", default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
 SECURE_BROWSER_XSS_FILTER = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
