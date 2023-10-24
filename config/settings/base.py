8f2a5599babff986227bc63e2184d07e0a2dc95c
Mateus Henrique de Paula <61089459+MateusHnr1@users.noreply.github.com>
2023-10-24 14:51:42 -0300

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -222,7 +222,7 @@ CSRF_COOKIE_HTTPONLY = env.bool("DJANGO_CSRF_COOKIE_HTTPONLY", default=True)
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-browser-xss-filter
 SECURE_BROWSER_XSS_FILTER = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#x-frame-options
-X_FRAME_OPTIONS = "DENY"
+X_FRAME_OPTIONS = ""
 
 # EMAIL
 # ------------------------------------------------------------------------------
